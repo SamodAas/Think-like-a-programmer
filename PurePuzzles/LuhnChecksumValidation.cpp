ffa3618683a4c1e79fb4d12917619bae16e13059
SamodAas <adomas.nasutavicius@gmail.com>
2021-10-20 08:00:07 +0200

    LuhnChecksumValidation

--- PurePuzzles/LuhnChecksumValidation.cpp
+++ PurePuzzles/LuhnChecksumValidation.cpp
@@ -19,27 +19,38 @@ STEP 2:
 int main(void){
 
     char digit;
-    int sum= 0;
+    int sum;
+    int sumEven = 0;
+    int sumOdd = 0;
+    int i = 1;
+    int doubleDigit;
     
     cout << "Enter card number: ";
-      
-    // reading individual digits to sum without doubleing anything and checking if its % 10 = 0;
-    for (int i = 0; i < 6; i++){
-        cin >> digit;
+    digit = cin.get();
+    
+    while (digit != 10){
+        if (digit == 10) break;
+        // Checking sum for both cases of card number being odd or even 
+        if (i % 2 != 0){
+            //splitting and doubling the digit            
+            doubleDigit = (digit-'0') * 2;
+            sumEven = (doubleDigit >= 10) ? sumEven + 1 + doubleDigit % 10 : sumEven + doubleDigit;
+            sumOdd += (digit - '0');
+        }
+        else {
+            doubleDigit = (digit-'0') * 2;
+            sumOdd = (doubleDigit >= 10) ? sumOdd + 1 + doubleDigit % 10 : sumOdd + doubleDigit;
+            sumEven += (digit - '0');
+        }
+        digit = cin.get();
+        i++;
+    }  
 
-        sum += digit - '0';
-    }    
+    if (i % 2 != 0) sum = sumEven;  
+    else sum = sumOdd;
 
-    if (sum % 10 == 0){
-        cout << "valid"; 
-    }
-    else {
-        cout << sum;
-    }
-    // Splitting double digit 
-    int doubleDigit = digit * 2;
-    sum = (doubleDigit >= 10) ? 1 + doubleDigit % 10 : doubleDigit;
-
+    if (sum % 10 == 0) cout << "valid"; 
+    else cout << "invalid";
 
     return 0;
 }
