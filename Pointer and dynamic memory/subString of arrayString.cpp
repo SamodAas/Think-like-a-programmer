15bd66512252b993bc7fd7409f4a9519e68d9d07
adomazzz <adomazzz@gmail.com>
2023-03-17 17:03:28 +0100

    Substring, replace arrayString

--- Pointer and dynamic memory/subString of arrayString.cpp
+++ Pointer and dynamic memory/subString of arrayString.cpp
@@ -3,6 +3,10 @@ using std::cout;
 using std::cin;
 using std::string;
 
+
+// Task is to write a function for getting a substring and function to replace target text with smth else.
+
+
 typedef char* arrayString;
 
 arrayString subString(arrayString og, int index, int length){
@@ -17,14 +21,91 @@ arrayString subString(arrayString og, int index, int length){
 
 
 
+int length(arrayString str){
+    int count = 0;
+    while (str[count]!= 0) {
+        count++;
+    }
+    return count;
+}
+
+
+void addChar(arrayString& sc, char c){
+    int oldLenght = length(sc);
+    arrayString newString = new char[oldLenght+2];
+
+    for (int i = 0; i < oldLenght;i++){
+        newString[i] = sc[i];
+    }
+    newString[oldLenght] = c;
+    newString[oldLenght+1]= 0;
+    delete[] sc;
+    sc = newString;
+}
+
+
+void concatenate(arrayString& sc, arrayString addedString){
+    int ogLength = length(sc);
+    int addedLength = ogLength+length(addedString);
+
+    arrayString newString = new char [addedLength+ogLength];
+    for (int i = 0; i < ogLength; i++){
+        newString[i] = sc[i];
+    }
+
+    for (int i = 0; i <addedLength; i++){
+        newString[i+ogLength] = addedString[i];
+    }
+    newString[addedLength+ogLength] = 0;
+
+    delete[] sc;
+    sc = newString;
+}
+
+void replaceString(arrayString& sc, arrayString target, arrayString replaceText){
+    int strLength = length(sc);
+    int targetLength = length(target);
+    int replaceLength = length(replaceText);
+    bool targetFound = true;
+
+    arrayString final = new char[1];
+    final[0]=0;
+    
+    
+
+    for (int i = 0; i < strLength; i++){
+        if (sc[i]==target[0]){
+            for (int j = 1; j <targetLength; j++){
+                if (sc[i+j]!=target[j]){
+                    targetFound = false;
+                    continue;
+                }
+            }
+            if (targetFound){
+                concatenate(final, replaceText);
+                i+=targetLength;
+            }
+        targetFound = true;
+        }
+        else{
+            addChar(final, sc[i]);
+        }
+     
+}
+    cout << final;
+}
+
 int main(){
     arrayString word = new char[9];
-    string smth = "abcdefgh";
+    arrayString test = new char[3];
+    test[0]= 'j',test[1] = 'o',test[2] = 0;
+    string smth = "abcdabgh";
     for (int i = 0; i <9; i++){
         word[i] = smth[i];
     }
     word[9]=0;
-    cout << subString(word, 3, 4);
+    concatenate(word, test);
+    replaceString(word, "ab", "qwr");
     
     
 
